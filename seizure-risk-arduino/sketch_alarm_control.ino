5923fa87504cfbc8fd4f2335f110f52206b440c2
kuantumleap123 <k3v_w1l@yahoo.com>
2019-09-22 12:17:28 +0700

    Add files via upload

---
+++ seizure-risk-arduino/sketch_alarm_control.ino
@@ -0,0 +1,24 @@
+const byte alarmSwitchPin = 2;
+const byte alarmPin = 7;
+int val = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(alarmSwitchPin,INPUT);
+  pinMode(alarmPin,OUTPUT);
+  digitalWrite(alarmSwitchPin,LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = digitalRead(alarmSwitchPin);
+  if(val == HIGH) {
+    //play alarm tone here
+    tone(alarmPin,500,500);
+    delay(500);
+  }
+  noTone(alarmPin);
+  delay(1000);
+}
+
